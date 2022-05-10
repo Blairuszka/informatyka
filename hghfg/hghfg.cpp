376da7e2d2323d6eb2c5abf233047ff93726d80c
Blairuszka <jennyelewen@gmail.com>
2022-05-10 10:00:17 +0200

    Komentarz

---
+++ hghfg/hghfg.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void fb(int f) {
+    int f0 = 0;
+    int f1 = 1;
+    for (int i = 0; i < f; i++) {
+        cout << f0 << "";
+        f1 += f0;
+        f0 = f1 - f0;
+    }
+}
+
+int main()
+{
+    int f;
+    cout << "ile wyrazów wypisać:";
+    cin >> f;
+    fb(f);
+}
+
